8a285749a203d3847516c4ba38b683ffcbb730b3
Olena Liubynetska <olena.liuby@gmail.com>
2023-11-04 14:08:09 +0000

    feat: Register models in admin panel

--- core_social/admin.py
+++ core_social/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core_social.models import Post, Profile, Comment, Like, FollowingRelationships
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(FollowingRelationships)
