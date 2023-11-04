25c161f554cf2f22f73c1cf801aeb1a1801068b2
Olena Liubynetska <olena.liuby@gmail.com>
2023-11-04 20:29:43 +0000

    feat: Add ProfileFollowersView & ProfileFollowingView with corresponding endpoints

--- core_social/urls.py
+++ core_social/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from core_social.views import ProfileViewSet, CurrentUserProfileView
+from core_social.views import (
+    ProfileViewSet,
+    CurrentUserProfileView,
+    ProfileFollowersView,
+    ProfileFollowingView,
+)
 
 router = DefaultRouter()
 router.register(r"profiles", ProfileViewSet, basename="profiles")
@@ -9,6 +14,8 @@ router.register(r"profiles", ProfileViewSet, basename="profiles")
 urlpatterns = [
     path("", include(router.urls)),
     path("me/", CurrentUserProfileView.as_view(), name="me"),
+    path("me/followers/", ProfileFollowersView.as_view(), name="me_followers"),
+    path("me/following/", ProfileFollowingView.as_view(), name="me_following"),
 ]
 
 app_name = "core_social"
--- core_social/views.py
+++ core_social/views.py
@@ -1,7 +1,11 @@
 from django.db.models import Count, Q
 from rest_framework import mixins, status
 from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    get_object_or_404,
+    RetrieveUpdateDestroyAPIView,
+    ListAPIView,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
@@ -12,6 +16,8 @@ from core_social.serializers import (
     ProfileSerializer,
     ProfileListSerializer,
     ProfileDetailSerializer,
+    FollowerRelationshipSerializer,
+    FollowingRelationshipSerializer,
 )
 
 
@@ -127,3 +133,23 @@ class ProfileViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericVi
                 {"detail": "You are not following this user."},
                 status=status.HTTP_404_NOT_FOUND,
             )
+
+
+class ProfileFollowersView(ListAPIView):
+    serializer_class = FollowerRelationshipSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.profile.followers.all()
+
+
+class ProfileFollowingView(ListAPIView):
+    serializer_class = FollowingRelationshipSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.profile.following.all()
