f0c68c624710c797c8ebf8c9be44d62bd2de580c
Olena Liubynetska <olena.liuby@gmail.com>
2023-11-04 20:19:52 +0000

    feat: Add follow & unfollow functionality, update ProfileViewSet & CurrentUserProfileView

--- core_social/serializers.py
+++ core_social/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
-from .models import Profile
+from rest_framework.generics import get_object_or_404
+
+from .models import Profile, FollowingRelationships
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     """Serializer for Profile model with update method for profile image"""
+
     user_email = serializers.EmailField(source="user.email", read_only=True)
+    followers_count = serializers.IntegerField(read_only=True)
+    following_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Profile
@@ -18,6 +23,8 @@ class ProfileSerializer(serializers.ModelSerializer):
             "phone_number",
             "birth_date",
             "bio",
+            "followers_count",
+            "following_count",
         )
 
     def update(self, instance, validated_data):
@@ -28,6 +35,54 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 
 class ProfileListSerializer(ProfileSerializer):
+    followed_by_me = serializers.SerializerMethodField()
+
     class Meta:
         model = Profile
-        fields = ("id", "profile_image", "full_name", "username")
+        fields = ("id", "profile_image", "full_name", "username", "followed_by_me")
+
+    def get_followed_by_me(self, obj):
+        request = self.context.get("request", None)
+        if request is not None:
+            user_profile = get_object_or_404(Profile, user=request.user)
+            return obj.followers.filter(follower=user_profile).exists()
+        return False
+
+
+class FollowingRelationshipSerializer(serializers.ModelSerializer):
+    profile_id = serializers.IntegerField(source="following.id", read_only=True)
+    username = serializers.CharField(source="follower.username")
+
+    class Meta:
+        model = FollowingRelationships
+        fields = ("profile_id", "username")
+
+
+class FollowerRelationshipSerializer(serializers.ModelSerializer):
+    profile_id = serializers.IntegerField(source="follower.id", read_only=True)
+    username = serializers.CharField(source="following.username")
+
+    class Meta:
+        model = FollowingRelationships
+        fields = ("profile_id", "username")
+
+
+class ProfileDetailSerializer(ProfileSerializer):
+    followers = FollowerRelationshipSerializer(many=True, read_only=True)
+    following = FollowingRelationshipSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = (
+            "id",
+            "profile_image",
+            "user_email",
+            "username",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "birth_date",
+            "bio",
+            "followers",
+            "following",
+        )
--- core_social/views.py
+++ core_social/views.py
@@ -1,12 +1,18 @@
-from rest_framework import mixins
+from django.db.models import Count, Q
+from rest_framework import mixins, status
+from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from core_social.models import Profile
-from core_social.serializers import ProfileSerializer, ProfileListSerializer
+from core_social.models import Profile, FollowingRelationships
+from core_social.serializers import (
+    ProfileSerializer,
+    ProfileListSerializer,
+    ProfileDetailSerializer,
+)
 
 
 class CurrentUserProfileView(RetrieveUpdateDestroyAPIView):
@@ -14,13 +20,19 @@ class CurrentUserProfileView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return (
+            Profile.objects.filter(user=self.request.user)
+            .prefetch_related("following", "followers")
+            .annotate(
+                followers_count=Count("followers"), following_count=Count("following")
+            )
+        )
+
     def get_object(self):
-        """Get user profile"""
-        user_profile = get_object_or_404(Profile, user=self.request.user)
-        return user_profile
+        return get_object_or_404(self.get_queryset())
 
     def destroy(self, request, *args, **kwargs):
-        """Delete user and profile"""
         profile = self.get_object()
         user = profile.user
         response = super().destroy(request, *args, **kwargs)
@@ -34,21 +46,84 @@ class ProfileViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericVi
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProfileListSerializer
+        if self.action == "retrieve":
+            return ProfileDetailSerializer
+        return ProfileListSerializer
+
     def get_queryset(self):
-        """Get list of profiles and filter by username and name"""
         queryset = super().get_queryset()
 
         username = self.request.query_params.get("username")
         first_name = self.request.query_params.get("first_name")
         last_name = self.request.query_params.get("last_name")
 
-        if username is not None:
+        if username:
             queryset = queryset.filter(user__username__icontains=username)
 
-        if first_name is not None:
+        if first_name:
             queryset = queryset.filter(first_name__icontains=first_name)
 
-        if last_name is not None:
+        if last_name:
             queryset = queryset.filter(last_name__icontains=last_name)
 
         return queryset
+
+    @action(
+        detail=True,
+        methods=["POST"],
+        url_path="follow",
+        permission_classes=[IsAuthenticated],
+        authentication_classes=[JWTAuthentication],
+    )
+    def follow(self, request, pk=None):
+        follower = get_object_or_404(Profile, user=request.user)
+        following = get_object_or_404(Profile, pk=pk)
+
+        if follower == following:
+            return Response(
+                {"detail": "You cannot follow yourself."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if FollowingRelationships.objects.filter(
+            follower=follower, following=following
+        ).exists():
+            return Response(
+                {"detail": "You are already following this user."},
+                status=status.HTTP_409_CONFLICT,
+            )
+
+        FollowingRelationships.objects.create(follower=follower, following=following)
+        return Response(
+            {"detail": "You started following this user."},
+            status=status.HTTP_204_NO_CONTENT,
+        )
+
+    @action(
+        detail=True,
+        methods=["POST"],
+        url_path="unfollow",
+        permission_classes=[IsAuthenticated],
+        authentication_classes=[JWTAuthentication],
+    )
+    def unfollow(self, request, pk=None):
+        follower = get_object_or_404(Profile, user=request.user)
+        following = get_object_or_404(Profile, pk=pk)
+
+        try:
+            relation = FollowingRelationships.objects.get(
+                Q(follower=follower) & Q(following=following)
+            )
+            relation.delete()
+            return Response(
+                {"detail": "You have unfollowed this user."},
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        except FollowingRelationships.DoesNotExist:
+            return Response(
+                {"detail": "You are not following this user."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
